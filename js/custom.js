ed01ac2504e712f30d8e8c7a6149cc12af73090d
FutBotManger Dev Team <info@futbotmanager.com>
2022-10-31 17:37:47 +0100

    Publish

---
+++ js/custom.js
@@ -0,0 +1,74 @@
+(function($) {
+    "use strict";
+	
+	/* ..............................................
+	Loader 
+    ................................................. */
+	
+	$(window).on('load', function() { 
+		$('.preloader').fadeOut(); 
+		$('#preloader').delay(550).fadeOut('slow'); 
+		$('body').delay(450).css({'overflow':'visible'});
+	});
+    	
+	/* ..............................................
+    Navbar Bar
+    ................................................. */
+	
+	$('.navbar-nav .nav-link').on('click', function() {
+		var toggle = $('.navbar-toggler').is(':visible');
+		if (toggle) {
+			$('.navbar-collapse').collapse('hide');
+		}
+	});
+	
+	/* ..............................................
+    Fixed Menu
+    ................................................. */
+    
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-header').addClass('fixed-menu');
+		} else {
+			$('.top-header').removeClass('fixed-menu');
+		}
+	});
+
+	/* ..............................................
+    Properties Filter
+    ................................................. */
+
+
+	/* ..............................................
+    Gallery
+    ................................................. */
+	
+	
+	
+	/* ..............................................
+    Scroll To Top
+    ................................................. */
+	
+	$(document).ready(function () {
+
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#scroll-to-top').fadeIn();
+			} else {
+				$('#scroll-to-top').fadeOut();
+			}
+		});
+
+		$('#scroll-to-top').click(function () {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
+
+	});
+	
+	
+
+	
+}(jQuery));
